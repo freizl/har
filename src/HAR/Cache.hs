a52703baa34212f7d8b7d7871ec045af88582146
Haisheng.Wu.Okta <haisheng.wu@okta.com>
2017-04-08 11:54:39 -0700

    Fix lint error - to be an newtype

--- src/HAR/Cache.hs
+++ src/HAR/Cache.hs
@@ -6,7 +6,7 @@ module HAR.Cache where
 import           Data.Aeson.TH
 import           Data.Text     (Text)
 
-data Cache = Cache { comment :: Maybe Text
+newtype Cache = Cache { comment :: Maybe Text
                    } deriving (Show)
 
 $(deriveJSON defaultOptions ''Cache)
